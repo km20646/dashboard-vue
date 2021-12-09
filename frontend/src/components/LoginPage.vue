026f599dec92b59e4cfaf1f9acc4866d8facd21d
jaehyuklee <>
2021-12-09 13:48:22 +0900

    backend npm install

---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>로그인 페이지</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import IndexPage from '@/components/IndexPage'
+import LoginPage from '@/components/LoginPage'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'IndexPage',
+    component: IndexPage
+  }, {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  }]
 })
